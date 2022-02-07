d3890f42e6f7c6b0807dfc7f1a5111f578351dae
Abhijit Mondal Abhi <abhi.m.abhijit@gmail.com>
2022-02-07 19:55:52 +0100

    Catalog http get

---
+++ Catalog/Controllers/ItemController.cs
@@ -0,0 +1,37 @@
+﻿using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemController : ControllerBase
+    {
+        private readonly InMemItemsRepository inMemItemsRepository;
+
+        public ItemController()
+        {
+            inMemItemsRepository = new InMemItemsRepository();
+        }
+
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            return inMemItemsRepository.GetItems();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Item> GetItem(Guid id)
+        {
+            var item = inMemItemsRepository.GetItem(id);
+
+            if(item is null)
+            {
+                return NotFound();
+            }
+
+            return item;
+        }
+    }
+}
---
+++ Catalog/Repositories/InMemItemsRepository.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.Entities;
+
+namespace Catalog.Repositories
+{
+    public class InMemItemsRepository
+    {
+        public readonly List<Item> items = new()
+        {
+            new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreateDate = DateTimeOffset.Now },
+            new Item { Id = Guid.NewGuid(), Name = "Iron Sowrd", Price = 20, CreateDate = DateTimeOffset.Now },
+            new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 18, CreateDate = DateTimeOffset.Now },
+        };
+
+        public IEnumerable<Item> GetItems()
+        {
+            return items;
+        }
+
+        public Item GetItem(Guid id)
+        {
+            return items.Where(x => x.Id == id).SingleOrDefault();
+        }
+    }
+}
